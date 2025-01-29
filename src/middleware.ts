e12142b88ad6af8c74d98c75ab685908d09f3a9d
ErRajeev <er.rajeev.dev@gmail.com>
2025-01-29 21:49:08 +0530

    bug fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { getToken } from 'next-auth/jwt';
 export default async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.AUTH_SECRET });
 
-  // console.log("token", token);
+  console.log("token in middleware", token);
   
   const { nextUrl } = req;
   const protectedRoutes = ["/contact", "/profile"];
@@ -26,6 +26,33 @@ export default async function middleware(req: NextRequest) {
   return NextResponse.next();
 }
 
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|auth).*)"],
-};
+
+// import getServerSession from "next-auth"
+// import { NextRequest, NextResponse } from "next/server";
+
+// export default async function middleware(req: NextRequest) {
+//   const session = getServerSession({ req, secret: process.env.AUTH_SECRET });
+//   console.log("Session:", session);  // Log the session to check
+
+//   const { nextUrl } = req;
+//   const protectedRoutes = ["/contact", "/profile"];
+//   const openRoutes = ["/home", "/blog"];
+
+//   const isAuthenticated = !!session?.user;  // Check if session user exists
+
+//   // Handle open routes
+//   if (openRoutes.includes(nextUrl.pathname)) {
+//     return NextResponse.next();
+//   }
+
+//   // Redirect unauthenticated users from protected routes
+//   if (protectedRoutes.includes(nextUrl.pathname) && !isAuthenticated) {
+//     return NextResponse.redirect(new URL("/auth", req.url));
+//   }
+
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|auth).*)"],
+// };
