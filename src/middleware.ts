fc7dd23c38eb3582ccfa045cc029d26e83123e3e
ErRajeev <er.rajeev.dev@gmail.com>
2025-01-30 10:03:33 +0530

    Fix Token Issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,29 +2,33 @@
 import { NextResponse, NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
-export default async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET , cookieName : "next-auth.session-token"});
 
-  console.log("token in middleware", token);
-  
-  const { nextUrl } = req;
-  const protectedRoutes = ["/contact", "/profile"];
-  const openRoutes = ["/home", "/blog"];
+const protectedRoutes = ["/contact", "/profile"];
+const openRoutes = ["/home", "/blog"];
+
+
+export default async function middleware(req: NextRequest) {
+
+  const secret = process.env.NEXTAUTH_SECRET
+  if (!secret) console.log("NEXTAUTH_SECRET is Missing");
+    
+  const token = await getToken({ req, secret: secret , cookieName : "next-auth.session-token"});
 
   const isAuthenticated = !!token;  
 
   // Handle open routes
-  if (openRoutes.includes(nextUrl.pathname)) {
-    return NextResponse.next();
-  }
+  if (openRoutes.includes(req.nextUrl.pathname)) return NextResponse.next();
+  
 
   // Redirect unauthenticated users from protected routes
-  if (protectedRoutes.includes(nextUrl.pathname) && !isAuthenticated) {
+  if (protectedRoutes.includes(req.nextUrl.pathname) && !isAuthenticated) {
     return NextResponse.redirect(new URL("/auth", req.url));
   }
 
   return NextResponse.next();
 }
+
+
 
 // import { NextResponse, NextRequest } from "next/server";
 
